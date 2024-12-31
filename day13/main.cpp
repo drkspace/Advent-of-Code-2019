222bbbacae53e4f484f73806b2cd2d6740c780bd
Daniel Kramer <drkspace@gmail.com>
2024-12-30 23:48:29 -0500

    Cleaned up the code and made output nicer

--- day13/main.cpp
+++ day13/main.cpp
@@ -39,6 +39,7 @@ void display(const std::unordered_map<P, IntCode::dt>& map, const IntCode::dt sc
 {
     std::println("Score: {}", score);
     std::array<std::array<char, 37>, 24> out{};
+    std::string out_str;
     for (const auto& [k,v]: map)
     {
         switch (v)
@@ -65,59 +66,11 @@ void display(const std::unordered_map<P, IntCode::dt>& map, const IntCode::dt sc
     {
         for (const auto& c: r)
         {
-            std::print("{}", c);
+            out_str += c;
         }
-        std::println("");
+        out_str += '\n';
     }
-}
-
-[[nodiscard]] P next_ball_loc(IntCode::Program prog, IntCode::dt ip, const P& paddle_pos)
-{
-    std::queue<IntCode::dt> tmp;
-    P prev_pos{}, cur_pos{};
-    while (!IntCode::halted(prog, ip)){
-
-        if (IntCode::next_is_inp(prog, ip))
-        {
-            tmp.push(0);
-            IntCode::run_till_io(prog, tmp, ip);
-        }
-        else if (IntCode::next_is_out(prog, ip))
-        {
-
-            const auto x = IntCode::run_till_io(prog, tmp, ip);
-            const auto y = IntCode::run_till_io(prog, tmp, ip);
-            const auto tid = IntCode::run_till_io(prog, tmp, ip);
-
-            if (tid == 4 && x == paddle_pos.x && (y+1) == paddle_pos.y)
-            {
-                continue;
-            }
-            if (tid == 4)
-            {
-                prev_pos = cur_pos;
-                cur_pos = {x,y};
-                std::println("Found ball @ {} (prev = {})", cur_pos, prev_pos);
-                // for (int i = 0; i<24; i++)
-                // {
-                //     for (int j = 0; j<37; j++)
-                //     {
-                //         std::print("{}", prog.program[639+i*37+j]);
-                //     }
-                //     std::println("");
-                // }
-            }
-            if ((tid == 4) && (y == paddle_pos.y || (y+1) == paddle_pos.y || ((y+2) == paddle_pos.y && cur_pos.y == prev_pos.y && std::abs(cur_pos.x-prev_pos.x) == 2)) )
-            {
-                return {x,y};
-            }
-            if (tid == 4 && y > paddle_pos.y)
-            {
-                break;
-            }
-        }
-    }
-    return {0,0};
+    std::print("{}", out_str);
 }
 
 int main(const int argc, char *argv[])
@@ -166,36 +119,29 @@ int main(const int argc, char *argv[])
 
         std::unordered_map<P, IntCode::dt> map;
         IntCode::dt score = 0;
-        P paddle_pos{}, target{}, ball_pos{}, prev_ball_pos{};
-        int frame = 0;
+        P paddle_pos{}, ball_pos{};
         IntCode::run_till_io(prog, tmp, ip);
         while (!IntCode::halted(prog, ip)){
             if (IntCode::next_is_inp(prog, ip))
             {
-                // auto next = next_ball_loc(prog, ip, paddle_pos);
 
                 if (ball_pos.x == paddle_pos.x)
                 {
                     tmp.push(0);
-                    std::println("Not moving");
                 } else if (ball_pos.x > paddle_pos.x)
                 {
                     tmp.push(1);
                     paddle_pos.x += 1;
-                    std::println("Moving right");
                 }else
                 {
                     tmp.push(-1);
                     paddle_pos.x -= 1;
-                    std::println("Moving left");
                 }
-                std::println("{}, {}", paddle_pos, ball_pos);
-
-                frame++;
-                std::println("Frame {}", frame);
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000/75));
+                std::print("\033[2J\033[H");
                 display(map, score);
-                std::println("=====================================");
                 IntCode::run_till_io(prog, tmp, ip);
+
             }
             else if (IntCode::next_is_out(prog, ip))
             {
@@ -215,14 +161,11 @@ int main(const int argc, char *argv[])
                     }
                     if (tid == 4)
                     {
-                        // std::println("Prev pos: {}", paddle_pos);
-                        prev_ball_pos = ball_pos;
                         ball_pos = {x,y};
-                        // std::println("Cur pos: {}", paddle_pos);
                     }
                 }
             }
         }
-        std::println("{}", score);
+        std::println("Final Score (Part 2): {}", score);
     }
 }
